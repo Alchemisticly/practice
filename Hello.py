871c7b57a74d9e907225d9f6315975e4ed391134
Alchemy <Alchemy@x-xs-macbook.local>
2017-12-10 13:50:20 -0500

    added ability to hold data from dogs api

--- Hello.py
+++ Hello.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
+from urllib2 import urlopen
+
 def main():
   print"Hello world"
+  getData()
+
+def getData():
+    url = "https://dog.ceo/api/breeds/list/all"
+    print(urlopen(url).read())
 
 if __name__=='__main__':
   main()
